50fc5825bd1ab4faf3f9dc05369be3059ab53f66
Shweta <35878561+shwetamurali@users.noreply.github.com>
2026-06-04 15:03:15 -0400

    [test-trigger] failure: trigger module test run

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 1.7.0"
   required_providers { aws = { source = "hashicorp/aws" } }
 }
-// [test-trigger] failure: orphaned bucket on teardown destroy
+// [test-trigger] failure: orphaned bucket on teardown destroy (module-test)
 resource "terraform_data" "destroy_failure" {
   count = var.fail_on_teardown ? 1 : 0
 
